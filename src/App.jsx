bbb96eba3deac18d4541d51c88437e7a0e6bd39d
sh0kh <shoxruuh07tuxtanazarov@gmail.com>
2025-01-21 18:11:15 +0500

    edit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import AppLayout from "./layouts/AppLayout";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import AdminLayout from "./layouts/AdminLayout";
+import ProtectedRoute from "./Components/ProtectedRoute"; // Импорт компонента защиты маршрутов
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AdminLayout />
+              </ProtectedRoute>
+            }
+          >
+
+          </Route>
+          <Route element={<MainLayout />}>
+            <Route index element={<Home />} />
+
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
